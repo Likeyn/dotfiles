92be108837868ff61e75b7f24c3599ca27471c41
Pierre-François Clement <likeyn@gmail.com>
2014-06-14 01:59:51 +0200

    Add a setup.sh file in an attempt to get a one-liner install

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+# Setup my preciousss dotfiles
+
+DIR="dotfiles"
+URL="https://github.com/Likeyn/dotfiles.git"
+
+# Tests if a given command exists
+exists () { command -v $1 &> /dev/null; }
+
+if [ -d $DIR ]; then
+	echo " * Folder $DIR already exists; aborting"
+	exit 1
+elif ! exists git; then
+  echo " * Cannot find `git`; aborting"
+	exit 2
+elif ! exists make; then
+	echo " * Cannot find `make`; aborting"
+	exit 2
+fi
+
+# All good, launching setup
+git clone $URL
+cd $DIR
+git submodule init
+make install
